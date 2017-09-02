7f32210891f216ecd96fa4bf80511aab62005e6d
ry00001 <hello@ry00001.me>
2017-09-02 19:06:23 +0000

    Second commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'switchpy',
+  packages = ['switchpy'],
+  version = '0.1',
+  description = 'Sane, easy command-line switch parsing for Python.',
+  author = 'ry00001',
+  author_email = 'hello@ry00001.me',
+  url = 'https://github.com/ry000000/switchpy',
+  download_url = 'https://github.com/ry00000/switchpy/archive/0.1.tar.gz',
+  keywords = ['switches', 'switch', 'parser'],
+  classifiers = [],
+)
